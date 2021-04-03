e37d515213b64ed40d45b9f41fa634c00f530400
Ilan Silva <ilansantos68@hotmail.com>
2021-04-03 17:58:26 -0300

    Adicionado dois novos recursos em customerRepository.js

--- repositories/customerRepository.js
+++ repositories/customerRepository.js
@@ -4,10 +4,19 @@ const create = (data) => {
     return customerModel.create(data)
 }
 
+const getAll = () => {
+    return customerModel.find()
+}
 
-
+const getByName = (name) => {
+    return customerModel.find({
+        name: `${name}`
+    })
+}
 module.exports = {
     create,
+    getAll,
+    getByName,
 
 }
 
