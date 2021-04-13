2314952cd81d44e2a306b0ea928fcb99f962bcad
Ilan Silva <ilansantos68@hotmail.com>
2021-04-12 22:26:49 -0300

    Revisado e corrigido os códigos de status do controller de pe

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,9 +7,8 @@ const Router = express.Router()
 const db = require('../database/database')
 
 Router.post('/createorder', async (req, res) => {
-    console.log(req.body)
     if (!req.body.products || !req.body.products || !req.body.client || !req.body.value){
-        res.status(404).json({message: "Sua requisição está com falta de dados, por favor verifique e tente novamente.", error: true, data: req.body})
+        res.status(200).json({message: "Sua requisição está com falta de dados, por favor verifique e tente novamente.", error: true, data: req.body})
         return
     }
     const client = await db.connect()
@@ -18,7 +17,7 @@ Router.post('/createorder', async (req, res) => {
         const findvalue = [req.body.client]
         const findcustomer = await db.query(findquery, findvalue)
         if (!findcustomer){
-            res.status(404).json({message: "Não foi localizado nenhum cliente com este ID, favor verifique e tente novamente.", error: true, data: req.body})
+            res.status(200).json({message: "Não foi localizado nenhum cliente com este ID, favor verifique e tente novamente.", error: true, data: req.body})
             return
         }
         
@@ -27,9 +26,8 @@ Router.post('/createorder', async (req, res) => {
         const createorder = await client.query(querytext, values)
 
 
-        res.status(200).json({message: 'Criação de pedido feito com sucesso!', error: false, data: createorder.rows})
+        res.status(201).json({message: 'Criação de pedido feito com sucesso!', error: false, data: createorder.rows})
     }catch(err){
-        console.log(err)
         res.status(404).json({message: 'Falha na criação do pedido!', error: true})
     }finally{
         client.release()
@@ -50,7 +48,6 @@ Router.get('/getorder', async (req, res) => {
                 res.status(200).json({message: `Localização feita com sucesso! foi recuperado ${getorder.rowCount} registros.`, error: false, data: getorder.rows})
             }
         }catch(err){
-            console.log(err)
             res.status(404).json({message: 'Falha ao recuperar dados!', error: true})
         }finally{
             client.release()
@@ -68,7 +65,6 @@ Router.get('/getorder', async (req, res) => {
                 res.status(200).json({message: `Localização feita com sucesso! foi recuperado ${getorder.rowCount} registros.`, error: false, data: getorder.rows})
             }
         }catch(err){
-            console.log(err)
             res.status(404).json({message: 'Falha ao recuperar dados!', error: true})
         }finally{
             client.release()
@@ -77,9 +73,8 @@ Router.get('/getorder', async (req, res) => {
 })
 
 Router.put('/updateorder', async (req, res) => {
-    console.log(req.body)
     if (!req.query.id){
-        res.status(404).json({message: "Não foi passado nenhum ID em sua requisição!", error: true})
+        res.status(200).json({message: "Não foi passado nenhum ID em sua requisição!", error: true})
         return
     }
     const client = await db.connect()
@@ -87,7 +82,7 @@ Router.put('/updateorder', async (req, res) => {
         // VALIDATE ID
         const vl_id = await client.query('SELECT SL_ID_PK FROM LOGISTIC.ORDERS WHERE SL_ID_PK = $1;', [req.query.id])
         if (vl_id.rowCount == 0){
-            res.status(404).json({message: "Não foi encontrado nenhum pedido com este ID!", error: true})
+            res.status(200).json({message: "Não foi encontrado nenhum pedido com este ID!", error: true})
         return
         }
         
@@ -113,12 +108,11 @@ Router.put('/updateorder', async (req, res) => {
         }
         const textatts = atts.join(", ")
         await client.query('COMMIT')
-        res.status(200).json({message: `${textatts} atualizado(s) com sucesso!`, error: false})
+        res.status(201).json({message: `${textatts} atualizado(s) com sucesso!`, error: false})
         
         
     }catch(err){
         await client.query('ROLLBACK')
-        console.log(err)
         res.status(404).json({message: "Falha ao atualizar o pedido!", error: true})
     }finally{
         client.release()
