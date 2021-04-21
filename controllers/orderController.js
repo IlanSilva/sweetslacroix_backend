70bf767a947a248fc321273b783310a6d0aa02fc
Ilan Silva <ilansantos68@hotmail.com>
2021-04-21 17:11:15 -0300

    Terminado end-points de pedidos

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -58,8 +58,8 @@ Router.post('/createorder', orderMiddleware.createValidation, async (req, res) =
 })
 
 
-Router.get('/getorder', async (req, res) => {
-    if (!req.query.id){
+Router.get('/getorder/:id', async (req, res) => {
+    if (!req.params.id){
         const client = await db.connect()
         try{
             const querytext = 'SELECT * FROM LOGISTIC.ORDERS;'
@@ -76,10 +76,10 @@ Router.get('/getorder', async (req, res) => {
             client.release()
         }
     }else{
-        const client = db.connect()
+        const client = await db.connect()
         try{
-            const querytext = 'SELECT * FROM LOGISTIC.ORDERS WHERE SL_ID_PK = $1;'
-            const values = [req.query.id]
+            const querytext = 'SELECT * FROM LOGISTIC.ORDERS WHERE OR_ID_PK = $1;'
+            const values = [req.params.id]
             const getorder = await client.query(querytext, values)
             // LÓGICA DE RETORNO BASEADO NA QUANTIDADE DE REGISTROS RECUPERADOS.
             if (getorder.rowCount <= 0){
@@ -95,52 +95,81 @@ Router.get('/getorder', async (req, res) => {
     }
 })
 
-Router.put('/updateorder', async (req, res) => {
-    if (!req.query.id){
-        res.status(200).json({message: "Não foi passado nenhum ID em sua requisição!", error: true})
+Router.put('/updateorder/:id',orderMiddleware.updateValidation, async (req, res) => {
+    const client = await db.connect()
+    // FIND ORDER
+    const query_find_order = await client.query('SELECT * FROM LOGISTIC.ORDERS WHERE OR_ID_PK = $1', [req.params.id])
+    if (query_find_order.rowCount < 1){
+        res.status(200).json({message: "Não foi encontrado nenhum pedido com este ID!", error: true})
+        return
+    }else if (query_find_order.rows[0].or_situation === 'CANCELADO' || query_find_order.rows[0].or_situation === 'SUSPENSO'){
+        res.status(200).json({message: "Não é possível alterar dados de um pedido que está suspenso ou cancelado!", error: true})
         return
     }
-    const client = await db.connect()
     try{
-        // VALIDATE ID
-        const vl_id = await client.query('SELECT SL_ID_PK FROM LOGISTIC.ORDERS WHERE SL_ID_PK = $1;', [req.query.id])
-        if (vl_id.rowCount == 0){
-            res.status(200).json({message: "Não foi encontrado nenhum pedido com este ID!", error: true})
-        return
+        // EMPACOTAÇÃO DOS PRODUTOS
+        const products = []
+        let pre_value = 0
+        const discount = 0
+        if (req.body.discount){
+            discount += req.body.discount
         }
-        
-        const atts = []
+        let productCount = 0
+        for (element of req.body.basket){
+            const productverify = await client.query('SELECT SL_PRODUCT_NAME, SL_PRODUCT_VALUE FROM LOGISTIC.PRODUCTS WHERE SL_ID_PK = $1;', [element.id])
+            if(productverify.rowCount > 0){
+                productCount += 1
+                pre_value += parseFloat(productverify.rows[0].sl_product_value)
+                products.push({index: productCount, name: productverify.rows[0].sl_product_name, value: productverify.rows[0].sl_product_value})
+                let pvst = pre_value.toFixed(2)
+                pre_value = parseFloat(pvst) 
+            }
+        }
+        const final_value = pre_value - (pre_value * discount / 100)
+
+        // START TRANSACTION
         await client.query('BEGIN')
-        if (req.body.products){
-            let querytext = 'UPDATE LOGISTIC.ORDERS SET SL_PRODUCTS = $1 WHERE SL_ID_PK = $2 RETURNING *;'
-            let values = [JSON.stringify(req.body.products), req.query.id]
-            let updateorder = await client.query(querytext, values)
-            atts.push('Produto(s)')
-        }
-        if (req.body.price){
-            let querytext = 'UPDATE LOGISTIC.ORDERS SET SL_PRICE = $1 WHERE SL_ID_PK = $2 RETURNING *;'
-            let values = [req.body.price, req.query.id]
-            let updateorder = await client.query(querytext, values)
-            atts.push('Preço')
-        }
-        if (req.body.situation){
-            let querytext = 'UPDATE LOGISTIC.ORDERS SET SL_SITUATION = $1 WHERE SL_ID_PK = $2 RETURNING *;'
-            let values = [req.body.situation, req.query.id]
-            let updateorder = await client.query(querytext, values)
-            atts.push('Situação')
-        }
-        const textatts = atts.join(", ")
+        
+        const text_createaddress = 'UPDATE LOGISTIC.ADDRESSES_FOR_ORDERS ' +
+        'SET AO_CEP = $1, AO_UF = $2, AO_CITY = $3, AO_NEIGHBORHOOD = $4, AO_STREET = $5, AO_NUMBERHOUSE = $6, AO_REFERENCE = $7, AO_DESCRIPTION = $8 WHERE AO_ID_PK = $9 RETURNING AO_ID_PK;'
+        const values_createaddress = [req.body.address.cep, req.body.address.uf, req.body.address.city, req.body.address.neighborhood, req.body.address.street, req.body.address.numberhouse,
+        req.body.address.reference, req.body.address.description, query_find_order.rows[0].or_address]
+        
+        const query_address = await client.query(text_createaddress, values_createaddress)
+        const text_updateorder = "UPDATE LOGISTIC.ORDERS SET OR_PRODUCTS = $1, OR_PRICE = $2 WHERE OR_ID_PK = $3 RETURNING OR_ID_PK;"
+        const values_updateorder = [{data: products}, final_value, req.params.id]
+        const query_order = await client.query(text_updateorder, values_updateorder)
         await client.query('COMMIT')
-        res.status(201).json({message: `${textatts} atualizado(s) com sucesso!`, error: false})
-        
-        
+        // END TRANSACTION
+
+        res.status(201).json({message: `Pedido atualizado com sucesso!`, error: false})
     }catch(err){
         await client.query('ROLLBACK')
         res.status(404).json({message: "Falha ao atualizar o pedido!", error: true})
     }finally{
         client.release()
     }
+})
 
+// SITUATION UPDATE
+Router.put('/situation/:id', orderMiddleware.updateSituationValidation, async (req, res) => {
+    const client = await db.connect()
+    try{
+        const query_find_order = await client.query('SELECT * FROM LOGISTIC.ORDERS WHERE OR_ID_PK = $1', [req.params.id])
+        if (query_find_order.rowCount < 1){
+            res.status(200).json({message: "Não foi encontrado nenhum pedido com este ID!", error: true})
+            return
+        }
+        const text_order_ups = "UPDATE LOGISTIC.ORDERS SET OR_SITUATION = $1 WHERE OR_ID_PK = $2 RETURNING OR_SITUATION;"
+        const values_ups = [req.body.order.situation, req.params.id]
+        const query = await client.query(text_order_ups, values_ups)
+
+        res.status(201).json({message: `Situação do pedido atualizado com sucesso para '${query.rows[0].or_situation}'`, error: false})
+    }catch(err){
+        res.status(404).json({message: "Falha ao atualizar o pedido!", error: true})
+    }finally{
+        client.release()
+    }
 })
 
 module.exports = Router
--- middlewares/validation/orderValidation.js
+++ middlewares/validation/orderValidation.js
@@ -58,6 +58,83 @@ const createValidation = async function (req, res, next){
         client.release()
     }
 }
+
+const updateValidation = async function(req, res, next){
+    const client = await db.connect()
+    try{
+        if (!req.params.id){
+            res.status(200).json({message: "Não foi passado nenhum ID em sua requisição!", error: true})
+            return
+        }
+        if (!req.body){
+            res.status(200).json({message: 'Não foi recebido nenhum dado no corpo da requisição!', error: true})
+            return
+        }
+        
+        // VALIDAR DADOS DE CLIENTE
+        if(!req.body.client.id){
+            res.status(200).json({message: 'Não foi recebido nenhum id de cliente em sua requisição!', error: true})
+            return
+        }else if(req.body.client.id){
+            const getclient = await client.query('SELECT SL_ID_PK FROM CUSTOMERS.PERSONS WHERE SL_ID_PK = $1;', [req.body.client.id])
+            if (getclient.rowCount > 0){
+                // EMPTY
+            }else{
+                res.status(200).json({message: `Não foi encontrado nenhum cliente com o ID ${req.body.client.id}!`, error: true})
+                return
+            }
+        }
+        // VALIDAR ITENS
+        if (!req.body.basket || req.body.basket.length < 1){
+            res.status(200).json({message: 'Não foi recebido nenhum produto em sua requisição!', error: true})
+            return
+        }else if(req.body.basket){
+            const arraytest = []
+            for (element of req.body.basket){
+                console.log('print element', element)
+                if(!element.id){
+                // EMPTY
+                }else{
+                    const productverify = await client.query('SELECT SL_ID_PK FROM LOGISTIC.PRODUCTS WHERE SL_ID_PK = $1;', [element.id])
+                    if(productverify.rowCount > 0){
+                        arraytest.push(element)
+                    }
+                }
+            }
+            if (arraytest.length != req.body.basket.length){
+                res.status(200).json({message: 'Há produtos em sua lista que não está válido!', error: true})
+                return
+            }
+        }
+        
+        //VALIDAR ENDEREÇO
+        if(!req.body.address.cep || !req.body.address.uf || !req.body.address.city || !req.body.address.neighborhood || !req.body.address.street){
+            res.status(200).json({message: 'Está faltando dados de endereço!', error: true})
+            return
+        }
+        next()
+    }catch(err){
+        console.log(err)
+        res.status(404).json({message: 'Houve um erro ao tentar realizar a validação de itens! ' + err, error: true})
+    }finally{
+        client.release()
+    }
+}
+
+const updateSituationValidation = async function (req, res, next){
+    if (!req.params.id){
+        res.status(200).json({message: "Não foi passado nenhum ID em sua requisição!", error: true})
+        return
+    }
+    if (!req.body.order.situation || req.body.order.situation.trim() === "" || !typeof req.body.order.situation === 'string'){
+        res.status(200).json({message: "Não foi passado nenhuma informação de situação em sua requisição!", error: true})
+        return
+    }
+    next()
+}
+
 module.exports = {
-    createValidation
+    createValidation,
+    updateValidation,
+    updateSituationValidation
 }
